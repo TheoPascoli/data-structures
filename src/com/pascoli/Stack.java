83c22c43239c653303d81d4e9e0dadc06bc26d03
Theo Pascoli <theo.pascoli@gmail.com>
2022-04-04 22:09:35 +0200

    add stack

---
+++ src/com/pascoli/Stack.java
@@ -0,0 +1,47 @@
+package com.pascoli;
+
+import java.util.EmptyStackException;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Stack <T> implements Iterable <T> {
+
+    private LinkedList<T> list = new LinkedList<>();
+
+    public Stack() {
+    }
+
+    public Stack(T firstElem) {
+        push(firstElem);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    // O(n)
+    public void push(T elem) {
+        list.addLast(elem);
+    }
+
+    // O(n)
+    public T pop() {
+        if (isEmpty()) throw new EmptyStackException();
+        return list.removeLast();
+    }
+
+    // O(n)
+    public T peek() {
+        if (isEmpty()) throw new EmptyStackException();
+        return list.peekLast();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
+}
