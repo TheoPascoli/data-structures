d941dad9383a553ef38f31804673c8b79983ce58
Theo Pascoli <theo.pascoli@gmail.com>
2022-04-13 23:02:01 +0200

    add queue

---
+++ src/com/pascoli/Queue.java
@@ -0,0 +1,43 @@
+package com.pascoli;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Queue<T> implements Iterable<T> {
+
+    private LinkedList<T> list = new LinkedList<>();
+
+    public Queue() {
+    }
+
+    public Queue(T firstElem) {
+        offer(firstElem);
+    }
+
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public T peek() {
+        if (isEmpty()) throw new RuntimeException("Queue Empty");
+        return list.peekFirst();
+    }
+
+    public T poll() {
+        if (isEmpty()) throw new RuntimeException("Queue Empty");
+        return list.removeFirst();
+    }
+
+    public void offer(T elem) {
+        list.addLast(elem);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return list.iterator();
+    }
+}
